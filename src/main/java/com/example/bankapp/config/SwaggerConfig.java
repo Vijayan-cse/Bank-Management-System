c20a67b7a052ce10245b595b43790499fc67cbab
VIJAYAN P <146182409+Vijayan-cse@users.noreply.github.com>
2026-06-27 23:10:35 +0530

    Add src/main/java/com/example/bankapp/config/SwaggerConfig.java

---
+++ src/main/java/com/example/bankapp/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.bankapp.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SecureBank API")
+                        .version("1.0")
+                        .description("Banking Application REST API Documentation")
+                        .contact(new Contact()
+                                .name("Vijayan")
+                                .email("vijayan@gmail.com")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
+    }
+}
